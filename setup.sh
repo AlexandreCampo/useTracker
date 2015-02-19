7e0ad24d98a6f74f136008c08e7ba890d9475e7b
Alexandre Campo <AlexandreCampo@users.noreply.github.com>
2015-02-19 05:33:34 +0100

    linux debian/ubuntu install script, bugfix

--- setup.sh
+++ setup.sh
@@ -10,17 +10,20 @@ function error
 
 # make sure the following dependencies have been installed 
 echo The setup program of the USE Tracker is about to update your system
+echo
+echo
+sudo echo Updating, please wait...
 sudo apt-get -qq update
 sudo apt-get -qq upgrade
 
 echo
 echo
 echo "Installing Dependencies"
-sudo apt-get install g++ libwxbase3.0-dev libopencv-dev libboost-program-options-dev libwxgtk3.0-dev libwxgtk-media3.0-dev freeglut3.0-dev libva-dev libbz2-dev libx264-dev || error "$LINENO: Aborted, could not install the required dependencies."
+sudo apt-get -yes install g++ libwxbase3.0-dev libopencv-dev libboost-program-options-dev libwxgtk3.0-dev libwxgtk-media3.0-dev freeglut3-dev libva-dev libbz2-dev libx264-dev || error "$LINENO: Aborted, could not install the required dependencies."
 
 echo
 echo
-echo "Building USE Tracker
+echo "Building USE Tracker"
 make release
 
 # install images
