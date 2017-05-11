9d24b8e054ffce6f0fa30bc110b8ac2fec83cdc2
Alexandre Campo <alexandre.campo@gmail.com>
2017-05-11 14:50:46 +0200

    ...

--- update.sh
+++ update.sh
@@ -26,7 +26,7 @@ wxversion=3.0
 # make sure you all necessary packages installed
 if [ $error -eq 0 ]; then
 zenity --info --text "About to update required package dependencies"
-(echo "# Updating packages..."; pkexec apt-get --yes install build-essential cmake libwxbase$wxversion-dev libopencv-dev libboost-program-options-dev libboost-filesystem-dev libwxgtk$wxversion-dev libwxgtk-media$wxversion-dev freeglut3-dev libva-dev libbz2-dev libx264-dev libbluetooth-dev
+(echo "# Updating packages..."; pkexec apt-get --yes install build-essential cmake premake4 libwxbase$wxversion-dev libopencv-dev libboost-program-options-dev libboost-filesystem-dev libwxgtk$wxversion-dev libwxgtk-media$wxversion-dev freeglut3-dev libva-dev libbz2-dev libx264-dev libbluetooth-dev
 ) |
 zenity --progress \
   --title="Updating package dependencies for USE Tracker" \
